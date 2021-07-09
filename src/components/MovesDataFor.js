4810dad56953a2ee9bdf59d59ea146ffc00e9df2
mr-atta <mohammadatta97@gmail.com>
2021-07-09 13:52:48 +0300

    lab 10

--- src/components/MovesDataFor.js
+++ src/components/MovesDataFor.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
-import { Container, Row, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 // //////////////////////////////////////////////////
 
